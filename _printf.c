f8f5634b15ba0d6c6693acedbd00d7101c5b7a58
Kenechukwu Akubue <kengoon19@gmail.com>
2023-11-13 14:24:05 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -37,7 +37,7 @@ int _printf(const char *format, ...)
 				i++;
 			}
 
-			else if (format[i + 1] == '%')
+			else if (format[i + 1] == '%' || format[i + 1] == '\0')
 			{
 				write(1, "%", 1);
 				len++;
