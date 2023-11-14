7f0dfc5c891f2a8b1d12e4cfa59e983667ea9dd3
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-11-14 21:06:17 +0000

    protoypes

--- main.h
+++ main.h
@@ -18,6 +18,16 @@ int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
+int print_char(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
 
 #endif
 
