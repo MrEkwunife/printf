87382245f83ada71e74360a98b696c401595d37f
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-11-14 22:05:02 +0000

    all

--- print_primitives.c
+++ print_primitives.c
@@ -167,7 +167,7 @@ int print_binary(va_list types, char buffer[],
 	SUPPRESS(size);
 
 	n = va_arg(types, unsigned int);
-	m = 2147483648; 
+	m = 2147483648;
 	a[0] = n / m;
 	for (i = 1; i < 32; i++)
 	{
