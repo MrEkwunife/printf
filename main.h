d4095e4e757d073baabb7510ce1b45b5729f897f
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-11-14 18:46:33 +0000

    prototype for get_size

--- main.h
+++ main.h
@@ -9,6 +9,7 @@ void print_buffer(char buffer[], int *buff_ind);
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
 #endif
 
