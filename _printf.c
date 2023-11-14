f84ac08d11a9ce9662d0e81bbf17b4766dd7f4c0
Kenechukwu Akubue <kengoon19@gmail.com>
2023-11-14 18:49:47 +0000

    delegate_print.c

--- _printf.c
+++ _printf.c
@@ -34,7 +34,7 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = delegate_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
