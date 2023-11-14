1a607378ba8dcc3fea6736ceb05ae7dc1482a1a0
Chimobi Ekwunife <ekwunifechimobi16@gmail.com>
2023-11-14 21:53:15 +0000

    prototypes

--- main.h
+++ main.h
@@ -28,6 +28,41 @@ int print_int(va_list types, char buffer[],
         int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
         int flags, int width, int precision, int size);
+int is_printable(char c);
+int append_hexa_code(char ascii_code, char buffer[], int i);
+int is_digit(char c);
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
+int write_char(char c, char buffer[],
+        int flags, int width, int precision, int size);
+int write_number(int is_negative, int ind, char buffer[],
+        int flags, int width, int precision, int size);
+int write_num(int ind, char buffer[],
+        int flags, int width, int prec,
+        int length, char padd, char extra_c);
+int write_unsgnd(int is_negative, int ind,
+        char buffer[],
+        int flags, int width, int precision, int size);
+int write_pointer(char buffer[], int ind, int length,
+        int width, int flags, char padd, char extra_c, int padd_start);
+int print_unsigned(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_octal(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_hexa_upper(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_hexa(va_list types, char map_to[], char buffer[],
+        int flags, char flag_ch, int width, int precision, int size);
+int print_pointer(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_non_printable(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_reverse(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_rot13string(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
 
 #endif
 
